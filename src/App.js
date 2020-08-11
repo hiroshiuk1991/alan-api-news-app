b29f7ef2ad5cb2f5b2a2d323ba389a773aa68496
hiroshiuk1991 <hiroshis91@icloud.com>
2020-08-12 00:05:20 +0100

    first
    api call created  and initial speech call.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alanKey = '4c09eaf59fa1dd15e14b29dbc403c4502e956eca572e1d8b807a3e2338fdd0dc/stage';
+
+const App = () => {
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles }) => {
+                if (command === 'newHeadlines') {
+                    console.log(articles);
+                }
+            }
+        })
+    }, [])
+
+    return (
+        <div> 
+            <h1> Alan AI News App.</h1>
+        </div>
+    )
 }
 
 export default App;
