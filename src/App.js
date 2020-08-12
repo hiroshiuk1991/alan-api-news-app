da3bfcff44bba18b8f803e01ae9c463ad19f1401
hiroshiuk1991 <hiroshis91@icloud.com>
2020-08-12 15:33:40 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web'
+
+import NewsCards from './components/NewsCards/NewsCards';
 
 const alanKey = '4c09eaf59fa1dd15e14b29dbc403c4502e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
+    const [newsArticles, setNewsArticles] = useState([])
+
     useEffect(() => {
         alanBtn({
             key: alanKey,
             onCommand: ({ command, articles }) => {
                 if (command === 'newHeadlines') {
-                    console.log(articles);
+                    setNewsArticles(articles);
                 }
             }
         })
@@ -18,6 +22,7 @@ const App = () => {
     return (
         <div> 
             <h1> Alan AI News App.</h1>
+            <NewsCards articles={newsArticles}/>
         </div>
     )
 }
