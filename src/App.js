36b603b07fe3296b5097eab259ebfa90baddcdbe
hiroshiuk1991 <hiroshis91@icloud.com>
2020-08-12 15:39:21 +0100

    added styling
    new styling

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web'
 
 import NewsCards from './components/NewsCards/NewsCards';
+// import classes from '*.module.css';
+
+import useStyles from './styles.js'
 
 const alanKey = '4c09eaf59fa1dd15e14b29dbc403c4502e956eca572e1d8b807a3e2338fdd0dc/stage';
 
 const App = () => {
     const [newsArticles, setNewsArticles] = useState([])
+    const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
@@ -21,7 +25,7 @@ const App = () => {
 
     return (
         <div> 
-            <h1> Alan AI News App.</h1>
+            <div className={classes.logoContainer}> <img src="https://alan.app/voice/images/branding_page/logo-horizontal/grayscale/alan-logo-horizontal-grayscale.svg?f67bf3352084536120d8a7fa473bf492" className={classes.alanLogo} alt="alan logo"/></div>
             <NewsCards articles={newsArticles}/>
         </div>
     )
