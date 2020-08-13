e66cbd83831b05d1748d24f7f0d12d82a6c76cbf
hiroshiuk1991 <hiroshis91@icloud.com>
2020-08-13 11:30:24 +0100

    finished.
    finished AI voice app.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,61 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import alanBtn from '@alan-ai/alan-sdk-web'
 
-import NewsCards from './components/NewsCards/NewsCards';
+import wordsToNumbers from 'words-to-numbers'
+
+import NewsCards from './components/NewsCards/NewsCards'
 // import classes from '*.module.css';
 
 import useStyles from './styles.js'
 
-const alanKey = '4c09eaf59fa1dd15e14b29dbc403c4502e956eca572e1d8b807a3e2338fdd0dc/stage';
+const alanKey =
+  '4c09eaf59fa1dd15e14b29dbc403c4502e956eca572e1d8b807a3e2338fdd0dc/stage'
 
 const App = () => {
-    const [newsArticles, setNewsArticles] = useState([])
-    const classes = useStyles();
+  const [newsArticles, setNewsArticles] = useState([])
+  const [activeArticle, setActiveArticle] = useState(-1)
+  const classes = useStyles()
 
-    useEffect(() => {
-        alanBtn({
-            key: alanKey,
-            onCommand: ({ command, articles }) => {
-                if (command === 'newHeadlines') {
-                    setNewsArticles(articles);
-                }
-            }
-        })
-    }, [])
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles, number }) => {
+        if (command === 'newHeadlines') {
+          setNewsArticles(articles)
+          setActiveArticle(-1)
+        } else if (command === 'highlight') {
+          setActiveArticle(prevActiveArticle => prevActiveArticle + 1)
+        } else if (command === 'open') {
+          const parsedNumber =
+            number.length > 2 ? wordsToNumbers((number), { fuzzy: true }) : number;
+          const article = articles[parsedNumber - 1]
 
-    return (
-        <div> 
-            <div className={classes.logoContainer}> <img src="https://alan.app/voice/images/branding_page/logo-horizontal/grayscale/alan-logo-horizontal-grayscale.svg?f67bf3352084536120d8a7fa473bf492" className={classes.alanLogo} alt="alan logo"/></div>
-            <NewsCards articles={newsArticles}/>
-        </div>
-    )
+          if (parsedNumber > 20) {
+            alanBtn().playText('Please try that again...')
+          } else if (article) {
+            window.open(article.url, '_blank')
+            alanBtn().playText('Opening...')
+          } else {
+            alanBtn().playText('Please try that again...')
+          }
+        }
+      }
+    })
+  }, [])
+
+  return (
+    <div>
+      <div className={classes.logoContainer}>
+        <img
+          src='https://alan.app/voice/images/branding_page/logo-horizontal/grayscale/alan-logo-horizontal-grayscale.svg?f67bf3352084536120d8a7fa473bf492'
+          className={classes.alanLogo}
+          alt='alan logo'
+        />
+      </div>
+      {/* <div>{!this.activeArticles.length ? <h2> say go back </h2> : null }</div> */}
+      <NewsCards articles={newsArticles} activeArticle={activeArticle} />
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -5,29 +5,29 @@ import useStyles from './styles.js'
 import NewsCard from '../NewsCard/NewsCard'
 
 const infoCards = [
-  { color: '#00838f', title: 'Latest News', text: 'Give me the latest news' },
+  { color: '#FF7F50', title: 'Latest News', text: 'Give me the latest news' },
   {
-    color: '#1565c0',
+    color: '#FF4500',
     title: 'News by Categories',
     info:
       'Business, Entertainment, General, Health, Science, Sports, Technology',
     text: 'Give me the latest Technology news'
   },
   {
-    color: '#4527a0',
+    color: '#FFA500',
     title: 'News by Terms',
     info: 'Bitcoin, PlayStation 5, Smartphones, Donald Trump...',
     text: "What's up with PlayStation 5"
   },
   {
-    color: '#283593',
+    color: '	#FF8C00',
     title: 'News by Sources',
     info: 'CNN, Wired, BBC News, Time, IGN, Buzzfeed, ABC News...',
     text: 'Give me the news from CNN'
   }
 ]
 
-const NewsCards = ({ articles }) => {
+const NewsCards = ({ articles, activeArticle }) => {
   const classes = useStyles()
 
   if (!articles.length) {
@@ -81,7 +81,7 @@ const NewsCards = ({ articles }) => {
       >
         {articles.map((article, i) => (
           <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: 'flex' }}>
-            <NewsCard article={article} i={i} />
+            <NewsCard article={article} activeArticle={activeArticle} i={i} />
           </Grid>
         ))}
       </Grid>
